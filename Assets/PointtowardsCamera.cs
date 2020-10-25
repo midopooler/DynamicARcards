a9319d0b6db3f17825c0dfce4dda1191bed7c33c
midopooler <midhapulkit28@gmail.com>
2020-10-25 19:18:51 +0530

    fixed unwanted light/shadows; added a scene for testing purposes only

--- Assets/PointtowardsCamera.cs
+++ Assets/PointtowardsCamera.cs
@@ -8,15 +8,13 @@ public class PointtowardsCamera : MonoBehaviour
     public Transform PlayerCard2;
     public Transform PlayerCard3;
 
+
     void Update()
     {
         // Rotate the camera every frame so it keeps looking at the target
-        transform.LookAt(PlayerCard);
-        transform.LookAt(PlayerCard2);
-        transform.LookAt(PlayerCard3);
-        // Same as above, but setting the worldUp parameter to Vector3.left in this example turns the camera on its side
-        transform.LookAt(PlayerCard, Vector3.left);
-        transform.LookAt(PlayerCard2, Vector3.left);
-        transform.LookAt(PlayerCard3, Vector3.left);
+        PlayerCard.transform.LookAt(this.gameObject.transform);
+        //transform.LookAt(PlayerCard2);
+        //transform.LookAt(PlayerCard3);
+       
     }
 }
