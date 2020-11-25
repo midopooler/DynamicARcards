8f29a08ba50f6928e8dfd74d3474ff83a6604abe
midopooler <midhapulkit28@gmail.com>
2020-11-25 21:50:01 +0530

    empty commit:ignore

---
+++ Assets/ADarkHorseMain/Scripts/LoadAPItoCard.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using TMPro;
+
+public class LoadAPItoCard : MonoBehaviour
+{
+    public string gitAPIurl;  //github testing API 
+    public string playerData;
+    public string FirstName;
+    public string LastName;
+    public int Number;
+    public int Goals;
+    public string AvatarURL;
+    public string clubLogoURL;
+
+    public Image profilepic;
+    public Image clubLogo;
+
+    public TextMeshProUGUI playerNumber;
+    public TextMeshProUGUI PlayerName;
+    public TextMeshProUGUI GoalsScored;
+    public TextMeshProUGUI place;
+
+
+
+    void Start()
+    {
+        LoadAPI();
+    }
+
+    public void LoadAPI()
+    {
+
+        StartCoroutine(Getplayerdata());
+
+    }
+
+    IEnumerator Getplayerdata()
+    {
+        UnityWebRequest webRequest = UnityWebRequest.Get(gitAPIurl);
+        yield return webRequest.SendWebRequest();
+
+        if (webRequest.isNetworkError)
+        {
+            Debug.Log(": Error: " + webRequest.error);
+        }
+        else
+        {
+            Debug.Log(":\nReceived: " + webRequest.downloadHandler.text);
+            playerData = webRequest.downloadHandler.text;
+            Debug.Log(playerData);
+
+            PlayerData data = JsonUtility.FromJson<PlayerData>(playerData);
+
+            FirstName = data.firstname;
+            LastName = data.lastname;
+            Debug.Log(data.firstname + data.lastname);
+            Number = data.specifics.number;
+            Goals = data.specifics.goals;
+            Debug.Log(Number);
+            Debug.Log(Goals);
+            AvatarURL = data.avatar;
+
+            PlayerName.text = FirstName + " " + LastName;
+            GoalsScored.text = "Goals Scored : " + Goals.ToString();
+            place.text = data.city + "," + data.state;
+            playerNumber.text = "#" + data.specifics.number.ToString();
+            clubLogoURL = data.specifics.club.logo;
+            Debug.Log(clubLogoURL);
+
+
+
+
+            StartCoroutine(GetTexture());
+        }
+    }
+    IEnumerator GetTexture()
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(AvatarURL);
+        UnityWebRequest www1 = UnityWebRequestTexture.GetTexture(clubLogoURL);
+        yield return www.SendWebRequest();
+        yield return www1.SendWebRequest();
+
+
+        if (www1.isNetworkError || www1.isHttpError)
+        {
+            Debug.Log(www1.error);
+
+        }
+
+        else if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Texture2D myTexture = ((DownloadHandlerTexture)www1.downloadHandler).texture;
+            clubLogo.sprite = Sprite.Create(myTexture, new Rect(0, 0, myTexture.width, myTexture.height), Vector2.one / 2);
+
+            Texture2D myTexture1 = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            profilepic.sprite = Sprite.Create(myTexture1, new Rect(0, 0, myTexture1.width, myTexture1.height), Vector2.one / 2);
+        }
+    }
+}
+
+
+[Serializable]
+public class PlayerData
+{
+    public string firstname;
+    public string lastname;
+    public specifics specifics;
+    public string avatar;
+    public string city;
+    public string state;
+
+}
+[Serializable]
+public class specifics
+{
+
+    public int goals;
+    public int number;
+    public int position;
+    public club club;
+
+}
+[Serializable]
+
+public class club
+{
+    public string name;
+    public string logo;
+}
+
+
+
+
