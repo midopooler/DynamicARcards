5206ee291f9190a7a61d9a97a22568e5dd433f7b
midopooler <midhapulkit28@gmail.com>
2020-12-09 14:54:15 +0530

    read description
    Added a navigation scene which can be used so that both the scenes can be built together

---
+++ Assets/ADarkHorseMain/Scripts/loader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loader : MonoBehaviour
+{
+   
+    public void Register()
+    {
+        Application.OpenURL("https://www.thedarkhorse.io/signup");
+    }
+    public void LoadLogin()
+    {
+        SceneManager.LoadScene("login");
+    }
+    public void LoadTableTop()
+    {
+        SceneManager.LoadScene("FeatheredPlanes");
+    }
+    public void LoadPlayAR()
+    {
+        SceneManager.LoadScene("ImageTracking");
+    }
+}
